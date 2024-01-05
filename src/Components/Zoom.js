b0dd06855447ac23888943cdf35b0cf38b50a943
vishwajit247 <vishwajitkhanna247@gmail.com>
2024-01-05 16:34:19 +0530

    final commit

--- src/Components/Zoom.js
+++ src/Components/Zoom.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './ZoomableContainer.css'; // Import your CSS file
+import React, { useState } from "react";
+import "./Zoom.css";
 import "./Contmain.css";
 import Button from "@mui/material/Button";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
@@ -7,6 +7,7 @@ import ContmainDetailBtn from "./Btn/ContmainDetailBtn";
 import ContmainSettingBtn from "./Btn/ContmainSettingBtn";
 import ContmainLegendBtn from "./Btn/ContmainLegendBtn";
 import ZoomOutMapIcon from "@mui/icons-material/ZoomOutMap";
+import ContButtonGroup from "./Contbtn";
 
 const ZoomableContainer = () => {
   const [isZoomed, setZoom] = useState(false);
@@ -16,19 +17,30 @@ const ZoomableContainer = () => {
   };
 
   return (
-      <div className={`zoom-container ${isZoomed ? 'zoomed' : ''}`}>
-          <div className="contmain-top">
-            <Button id="document-btn" variant="outlined" endIcon={<KeyboardArrowDownIcon />}> Related Documents</Button>
-             <ContmainDetailBtn />
-             <ContmainSettingBtn />
-          </div>
-       <div className="contmain-center">Under Development</div> 
-          <div className="contmain-bottom">
-              <ContmainLegendBtn />
-              <ZoomOutMapIcon onClick={toggleZoom} />
-         
+    <div className={`zoom-container ${isZoomed ? "zoomed" : ""}`}>
+      <div className="contmain-top">
+        <Button
+          id="document-btn"
+          variant="outlined"
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          {" "}
+          Related Documents
+        </Button>
+        <ContmainDetailBtn />
+        <ContmainSettingBtn />
+      </div>
+      <div className="contmain-center">Under Development</div>
+      <div className="contmain-bottom">
+        <ContmainLegendBtn />
+        <div className="contmain-bottom-right">
+          <ContButtonGroup />
+          <ZoomOutMapIcon
+            sx={{ backgroundColor: "#0077B6", fill: "#ffffff" }}
+            onClick={toggleZoom}
+          />
         </div>
-    
+      </div>
     </div>
   );
 };
